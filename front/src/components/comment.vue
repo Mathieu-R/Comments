3138d8e9e6d28e704fd0c26bbbf8aa1b07a7b14a
Mathieu <mathieu0709@gmail.com>
2016-08-12 17:40:30 +0200

    front

--- front/src/components/comment.vue
+++ front/src/components/comment.vue
@@ -1,13 +1,28 @@
 <template>
     <div class="comment">
-
+      <div class="avatar"><img :src="'https://www.gravatar.com/avatar/' + comment.mail" alt="avatar"></div>
+      <div class="content">
+        <div class="author">{{ comment.username }}</div>
+        <div class="metadata">
+          <span class="date">{{ comment.created_at }}</span>
+        </div>
+        <div class="text">
+          {{ comment.content }}
+        </div>
+        <div class="actions">
+          <a href="#">Répondre</a>
+        </div>
+      </div>
+      <div class="comments"></div>
+        <comment :comment="reply" v-for="reply in comment.replies" :ip="ip"></comment> <!-- Composant à l'intérieur de lui-même -->
     </div>
 </template>
 
 <script type=text/babel>
     import axios from 'axios'
 
-    export default{
+    export default {
+        name: comment,
         props: {
           ip: String,
           comment: Object
